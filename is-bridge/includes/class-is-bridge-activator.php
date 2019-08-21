9b8d4fe76f9d3da1039be98880120c22055e0855
Ryan Holt <ryanholt@Ryan-H-V182JK7M.lan>
2019-08-20 21:37:47 -0400

    Initial Commit

---
+++ is-bridge/includes/class-is-bridge-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://holtech.us
+ * @since      1.0.0
+ *
+ * @package    Is_Bridge
+ * @subpackage Is_Bridge/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Is_Bridge
+ * @subpackage Is_Bridge/includes
+ * @author     Ryan Holt <rholt@getuwired.com>
+ */
+class Is_Bridge_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
